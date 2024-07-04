2afd291f0583ac9bfca552642a57b4ca1c44a514
monykappa <kimmony039@gmail.com>
2024-07-04 11:49:58 +0700

    feat: Update allowed hosts and product card layout
    - Update allowed hosts in settings.py to include new IP address
    - Improve product card layout and styling in list view
    
    Closes #123

--- KhmerEmpireShop/settings.py
+++ KhmerEmpireShop/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 
 # Config the IP address of the server
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '172.16.100.158']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.89.205']
 
 # settings.py
 CORS_ALLOW_ALL_ORIGINS = True
