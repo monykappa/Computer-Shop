251131cfe9f0f5446bceef6d6c39f6f93a3a9674
monykappa <kimmony039@gmail.com>
2024-07-06 13:40:53 +0700

    feat: Update email backend configuration for SMTP
    This commit updates the email backend configuration in the settings.py file to use the SMTP backend for sending emails. The email host is set to 'smtp.gmail.com' with TLS enabled on port 587. The email host user and password are also updated.
    
    Closes #123

--- KhmerEmpireShop/settings.py
+++ KhmerEmpireShop/settings.py
@@ -76,6 +76,12 @@ CKEDITOR_CONFIGS = {
 }
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ('oudommonykim471@gmail.com')
+EMAIL_HOST_PASSWORD = ('monykappa$$')
 
 
 SOCIALACCOUNT_PROVIDERS = {
--- KhmerEmpireShop/urls.py
+++ KhmerEmpireShop/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
@@ -20,5 +20,10 @@ urlpatterns = [
     
     path('accounts/', include('allauth.urls')),
     
+    # reset password
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
 ]
